de660b6f972f370613d01df634385a74e3bb445d
PatrykKrzy <patryk_55@o2.pl>
2023-08-31 21:27:16 +0200

    New car controller and some models

---
+++ Assets/Scripts/CarControler/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+    [SerializeField] private CinemachineVirtualCamera virtualCamera;
+    [SerializeField] private PrometeoCarController carController;
+
+    private CinemachineTransposer _transposer;
+
+    void Start()
+	{
+        _transposer = virtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+	void LateUpdate()
+	{
+        float blend = carController.carSpeed / carController.maxSpeed; 
+        float damping = Mathf.Lerp(-10f, -20f, blend); 
+        _transposer.m_FollowOffset.z = damping;
+    }
+}
