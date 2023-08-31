3f8268cdd056f18e547e4948fe7e7e8ba7b35363
PatrykKrzy <patryk_55@o2.pl>
2023-08-31 22:49:10 +0200

    Camera following improvements

--- Assets/Scripts/CarControler/CameraFollow.cs
+++ Assets/Scripts/CarControler/CameraFollow.cs
@@ -5,7 +5,13 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
     [SerializeField] private PrometeoCarController carController;
+    [SerializeField] private Transform focusTransform;
 
+    [SerializeField] private Vector2 yOffsetRange;
+    [SerializeField] private Vector2 zOffsetRange;
+    [SerializeField] private Vector2 zFocusRange;
+    [SerializeField] private float smoothSpeed = 0.5f;
+    
     private CinemachineTransposer _transposer;
 
     void Start()
@@ -16,7 +22,17 @@ public class CameraFollow : MonoBehaviour
 	void LateUpdate()
 	{
         float blend = carController.carSpeed / carController.maxSpeed; 
-        float damping = Mathf.Lerp(-10f, -20f, blend); 
-        _transposer.m_FollowOffset.z = damping;
+
+        float dampingY = Mathf.Lerp(yOffsetRange.x, yOffsetRange.y, blend);
+        float smoothedDampingY = Mathf.Lerp(_transposer.m_FollowOffset.y, dampingY, smoothSpeed * Time.deltaTime);
+        _transposer.m_FollowOffset.y = smoothedDampingY;
+
+        float dampingZ = Mathf.Lerp(zOffsetRange.x, zOffsetRange.y, blend);
+        float smoothedDampingZ = Mathf.Lerp(_transposer.m_FollowOffset.z, dampingZ, smoothSpeed * Time.deltaTime);
+        _transposer.m_FollowOffset.z = smoothedDampingZ;
+
+        float focusZ = Mathf.Lerp(zFocusRange.x, zFocusRange.y, blend);
+        float smoothedFocusZ = Mathf.Lerp(focusTransform.localPosition.z, focusZ, smoothSpeed * Time.deltaTime);
+        focusTransform.localPosition = new Vector3(focusTransform.localPosition.x, focusTransform.localPosition.y, smoothedFocusZ);
     }
 }
